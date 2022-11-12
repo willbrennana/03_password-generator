e007716c8a57ccbc04278068243e41b932027aee
willbrennana <will.brennana@gmail.com>
2022-11-12 14:26:19 -0600

    Compiled Updates:
    11.6.1
    Added HTML, CSS & JavaScript files to start debugging.
    
    11.6.2
    Added variables to and pseudocode for function.
    
    11.11.1
    Started on function by adding if statements to account for all positives and negatives.
    
    11.11.2
    Continued to build function by converting input from string to number.
    Added password reset init based on Office Hours findings.
    
    11.11.3
    Created a “for” loop and added a “passwordString” variable to convert input back from number into string.
    
    11.11.4
    Debugged by including for loop and string within the final bracket.
    
    11.12.1
    Added text to ReadMe file and pushed commits to Github.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Password Generator
+This was my first time writing in JavaScript and second debugging challenge. 
+
+Debugging typically requires more thinking than doing—as often times the actual writing takes far less to complete than expected.
+
+Writing the meat and potatoes of the function felt a lot like examining fractals. Accounting for all the conditions in which a random password could be generated led me to a pattern that eventually made completing the function relatively quickly. 
+
+My biggest "aha" moments were realizing I had to convert the input from a string into a number—then back to a string—and add an init at the top of the function to reset the password field every time.
+
+Implementing a "for" loop and incorporating into the function was also revelatory, as the majority of my final debugging stage involved displaying the password within the box.
+
+As specified in the Challenge Acceptance Criteria, the generator includes the following:
+
+✨ A series of prompts for password criteria.
+
+✨ Criteria presented for selection to include in the password.
+
+✨ Criteria for length to be at least 8 characters and no more than 128 characters.
+
+✨ Criteria for whether or not to include lowercase, uppercase, numeric, and/or special characters.
+
+✨ Validated input for at least one character type to be selected.
+
+✨ Generated password that matches the selected criteria.
+
+✨ Password displayed to the page.
+
+Below is a screenshot of my latest and greatest:
+
+/Users/will/code/password-generator/images/PageScreenshot.png
+
+To generate your next secure password, visit [github.io link]
+
---
+++ script.js
@@ -0,0 +1,245 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
+// Add variables
+let password = [];
+let charset = {
+  lowerCase: [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ],
+  upperCase: [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ],
+  numerical: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  specialCharacters: [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.',
+  ]
+};
+let input = true;
+let choices = true;
+let hasLowerCase = true;
+let hasUpperCase = true;
+let hasNumerical = true;
+let hasSpecialCharacters = true;
+
+//Add function//
+function generatePassword() {
+  //Reset//
+  password = [];
+  //Convert string into number
+  input = Number(
+    prompt("How many characters are in your password? (8-128 characters)")
+  );
+  if (!input) {
+    alert("Please try again and specify your password length.");
+  } else if (input < 8 || input > 128) {
+    alert("Your password isn't quiiiite the right length.")
+  } else {
+    hasLowerCase = confirm("Would you like lowercase letters?");
+    hasUpperCase = confirm("Would you like uppercase letters?");
+    hasNumerical = confirm("Would you like numbers?");
+    hasSpecialCharacters = confirm("Would you like special characters?")
+    // Has 4 positives //
+    if (hasLowerCase && hasUpperCase && hasNumerical && hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(
+        charset.upperCase,
+        charset.numerical,
+        charset.specialCharacters
+      );
+    } // Has 3 positives & 1 negative //
+    else if (
+      hasLowerCase &&
+      hasUpperCase &&
+      hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(
+        charset.upperCase,
+        charset.numerical
+      );
+    } else if (
+      hasLowerCase &&
+      hasUpperCase &&
+      !hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(
+        charset.upperCase,
+        charset.specialCharacters
+      );
+    } else if (
+      hasLowerCase &&
+      !hasUpperCase &&
+      hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(
+        charset.numerical,
+        charset.specialCharacters
+      );
+    } else if (
+      !hasLowerCase &&
+      hasUpperCase &&
+      hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.upperCase.concat(
+        charset.numerical,
+        charset.specialCharacters
+      );
+    } // Has 2 positives & 2 negatives // 
+    else if (
+      hasLowerCase &&
+      hasUpperCase &&
+      !hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(charset.upperCase);
+    } else if (
+      hasLowerCase &&
+      !hasUpperCase &&
+      hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(charset.numerical);
+    } else if (
+      !hasLowerCase &&
+      hasUpperCase &&
+      hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.upperCase.concat(charset.numerical);
+    } else if (
+      !hasLowerCase &&
+      !hasUpperCase &&
+      hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.numerical.concat(charset.specialCharacters);
+    } else if (
+      !hasLowerCase &&
+      hasUpperCase &&
+      !hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.upperCase.concat(charset.specialCharacters);
+    } else if (
+      hasLowerCase &&
+      !hasUpperCase &&
+      !hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.lowerCase.concat(charset.specialCharacters);
+    }
+    // Has 1 positive & 3 negatives //
+    else if (
+      hasLowerCase &&
+      !hasUpperCase &&
+      !hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.lowerCase
+    } else if (
+      !hasLowerCase &&
+      !hasUpperCase &&
+      !hasNumerical &&
+      hasSpecialCharacters) {
+      choices = charset.specialCharacters
+    } else if (
+      !hasLowerCase &&
+      !hasUpperCase &&
+      hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.numerical
+    }
+    else if (
+      !hasLowerCase &&
+      hasUpperCase &&
+      !hasNumerical &&
+      !hasSpecialCharacters) {
+      choices = charset.upperCase
+    }
+    // Has 4 negatives //
+    else if (
+      !hasLowerCase &&
+      !hasUpperCase &&
+      !hasNumerical &&
+      !hasSpecialCharacters)
+      alert("Your password needs some work. Choose a criteria to start.")
+  }
+
+  //Create "for" loop to repeatedly push random password 
+  for (let i = 0; i < input; i++) {
+    let finalChoice = choices[Math.floor(Math.random() * choices.length)];
+    password.push(finalChoice);
+  }
+
+  //Convert to String
+  let passwordString = password.join("");
+  document.querySelector("#password").textContent = passwordString;
+
+}
